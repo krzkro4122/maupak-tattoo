c94247d64a23e1f407aef5c832f0a4b2897f8b12
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-03-29 19:24:37 +0200

    Contact page added. Working server+static files configuration i think. Have to finish the contact page styling still.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+import { readFile } from "fs/promises";
 import express from "express";
-const app = express();
-const PORT = process.env.PORT || 8000;
+import dotenv from "dotenv";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+dotenv.config();
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use("/", express.static(__dirname + "/pages/home"));
+app.use("/contact", express.static(__dirname + "/pages/contact"));
+
+const getFile = async (path) => {
+  try {
+    console.log(`Serving file: ${path}`);
+    return await readFile(path, "utf-8");
+  } catch (error) {
+    console.error(`ERROR: Couldn't open file: ${path}!`);
+    return error;
+  }
+};
+
+app.get("/", async (request, response) => {
+  response.send(await getFile("./pages/home/index.html"));
 });
 
+app.get("/contact", async (request, response) => {
+  response.send(await readFile("./pages/contact/index.html", "utf-8"));
+});
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Serving on http://localhost:${PORT}`);
 });
